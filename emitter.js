bbda81209f4da247df5a014bdb0b7d11fc143c8a
Saypiev Anvar <who-e@yandex.ru>
2017-10-28 15:53:21 +0500

    first commit

--- emitter.js
+++ emitter.js
@@ -15,30 +15,65 @@ function getEmitter() {
     return {
 
         /**
+         * асоциативный массив, ключами которого 
+         * являются события (например slide.funny),
+         * значениями - массив получателей.
+         * Получатель - {context: ?, handler: ?}
+         */
+        recipientsOfEvents: [],
+
+        /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            initRecipients.call(this, event);
+            this.recipientsOfEvents[event].push({
+                context,
+                handler
+            });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            Object.keys(this.recipientsOfEvents).forEach(eventRecipients => {
+                if (eventRecipients === event || eventRecipients.indexOf(`${event}.`) !== -1) {
+                    this.recipientsOfEvents[eventRecipients] =
+                    this.recipientsOfEvents[eventRecipients]
+                        .filter(recipient => recipient.context !== context);
+                }
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            do {
+                let recipients = this.recipientsOfEvents[event];
+                if (!recipients) {
+                    continue;
+                }
+                for (let indexRecipient of recipients.keys()) {
+                    handleEvent.call(this, event, indexRecipient);
+                }
+            } while ((event = getParentNamespace(event)));
+
+            return this;
         },
 
         /**
@@ -48,9 +83,17 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object}
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            initRecipients.call(this, event);
+            this.recipientsOfEvents[event].push({
+                context,
+                handler,
+                times
+            });
+
+            return this;
         },
 
         /**
@@ -60,9 +103,70 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object}
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            initRecipients.call(this, event);
+            this.recipientsOfEvents[event].push({
+                context,
+                handler,
+                frequency
+            });
+
+            return this;
         }
     };
+}
+
+/**
+ * 
+ * @param {String} event 
+ * @returns {String}
+ */
+function getParentNamespace(event) {
+    let indexLastDot = event.lastIndexOf('.');
+
+    return event.substring(0, indexLastDot);
+}
+
+/**
+ * 
+ * @this GetEmitter
+ * @param {String} event 
+ */
+function initRecipients(event) {
+    if (this.recipientsOfEvents[event] === undefined) {
+        this.recipientsOfEvents[event] = [];
+    }
+}
+
+
+/**
+ * 
+ * @this GetEmitter
+ * @param {String} event 
+ * @param {indexRecipient} indexRecipient
+ * @returns {Object}
+ */
+function handleEvent(event, indexRecipient) {
+    let recipient = this.recipientsOfEvents[event][indexRecipient];
+    if (!recipient.counter) {
+        recipient.counter = 0;
+    }
+    if (recipient.frequency) {
+        if (recipient.counter % recipient.frequency === 0) {
+            recipient.handler.call(recipient.context);
+        }
+    } else if (recipient.times) {
+        if (recipient.times > recipient.counter) {
+            recipient.handler.call(recipient.context);
+        } else {
+            this.recipientsOfEvents[event].splice(indexRecipient, 1);
+        }
+    } else {
+        recipient.handler.call(recipient.context);
+    }
+    recipient.counter++;
+
+    return this;
 }
